10e65a7c039cbb8395a2e26435adc8de68f8d669
Noah Over <nko2@duke.edu>
2016-11-01 23:39:31 -0400

    commented the SLOGOView interface

--- src/slogo_view/SLOGOView.java
+++ src/slogo_view/SLOGOView.java
@@ -15,6 +15,8 @@ public interface SLOGOView {
 
 	/**
 	 * Getter for the ResourceBundle for the language currently in use by the user
+	 * 
+	 * @return the ResouceBundle being used by this class
 	 */
 	public ResourceBundle getResourceBundle();
 
@@ -23,24 +25,75 @@ public interface SLOGOView {
 	 */
 	public void clearTrails();
 	
+	/**
+	 * Adds a user-defined command to the display on the screen
+	 * 
+	 * @param userCommand - the name of the user-defined command
+	 */
 	public void addUserCommand(String userCommand);
 	
+	/**
+	 * Adds a user-defined variable to the display on the screen
+	 * 
+	 * @param userVariable - the name of the variable
+	 * @param value - the value of the variable
+	 */
 	public void addUserVariable(String userVariable, double value);
 	
+	/**
+	 * Removes a user-defined variable from the display on the screen
+	 * 
+	 * @param userVariable - the name of the variable
+	 */
 	public void removeUserVariable(String userVariable);
 	
+	/**
+	 * Getter for the SLOGOController being used by the view
+	 * 
+	 * @return the SLOGOController being used by the view
+	 */
 	public SLOGOController getController();
 	
+	/**
+	 * Getter for the maximum value of x a turtle can move before it would be off the screen
+	 * 
+	 * @return the maximum value of x a turtle can move before it would be off the screen
+	 */
 	public double getMaxX();
 	
+	/**
+	 * Getter for the maximum value of y a turtle can move before it would be off the screen
+	 * 
+	 * @return the maximum value of y a turtle can move before it would be off the screen
+	 */
 	public double getMaxY();
 	
+	/**
+	 * Setter for the turtle's pen color
+	 * 
+	 * @param index - the index of the color to set the turtle's pen to
+	 */
 	public void setPenColor(int index);
 	
+	/**
+	 * Setter for the background color
+	 * 
+	 * @param index - the index of the color to set the background to
+	 */
 	public void setBackgroundColor(int index);
 	
+	/**
+	 * Setter for the image
+	 * 
+	 * @param index - the index of the image to set the turtle to
+	 */
 	public void setImage(int index);
 	
+	/**
+	 * Setter for the SLOGOController used by the view
+	 * 
+	 * @param controller - the controller to be used by the view
+	 */
 	public void setController(SLOGOController controller);
 		
 	/**
@@ -48,19 +101,54 @@ public interface SLOGOView {
 	 * changes have been made and the screen should update to reflect this
 	 */
 	public void updateScreen();
-
+	
+	/**
+	 * Setter for the turtle's pen's width
+	 * 
+	 * @param index - the width it will set the turtle's pen to
+	 */
 	public void setPenWidth(int index);
-
+	
+	/**
+	 * Getter for the color of the pen
+	 * 
+	 * @return the index of the color of the pen
+	 */
 	public int getPenColor();
-
+	
+	/**
+	 * Getter for the image of the turtle
+	 * 
+	 * @return the index of the image of the turtle
+	 */
 	public int getImage();
 	
+	/**
+	 * Getter for the size of the turtle's pen
+	 * 
+	 * @return the size of the turtle's pen
+	 */
 	public int getPenSize();
 	
+	/**
+	 * Getter for the background color
+	 * 
+	 * @return the index of the background color
+	 */
 	public int getBackground();
 	
+	/**
+	 * Getter for the language used by the view
+	 * 
+	 * @return the String which represents the language used by the view
+	 */
 	public String getLanguage();
 	
+	/**
+	 * Setter for the language used by the view
+	 * 
+	 * @param language - the name of the language to be used by the view
+	 */
 	public void setLanguage(String language);
 
 }
